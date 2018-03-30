30fb869278f363ca9947880132369a713802b71f
NullWir3 <raulch1998@gmail.com>
2018-03-29 23:58:46 -0400

    Lyrixs 1.0.0

---
+++ Lyrixs.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyLyrics import *
+import dbus
+
+
+def spotify_lyrics():
+    session_bus = dbus.SessionBus()
+    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
+                                         "/org/mpris/MediaPlayer2")
+    spotify_properties = dbus.Interface(spotify_bus,
+                                        "org.freedesktop.DBus.Properties")
+    metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player",
+                                      "Metadata")
+
+    # To just print the title
+    title = (metadata['xesam:title'])
+    artist = (metadata['xesam:artist'][0])
+
+    # print artist
+
+    def print_lyrics(song_title, song_singer):
+        return PyLyrics.getLyrics(song_singer, song_title)
+
+    try:
+        lyrics = print_lyrics(title, artist)
+
+    except ValueError:
+        try:
+            lyrics = print_lyrics(title.title(), artist.title())
+        except ValueError:
+            lyrics = 'Lyrics not found :('
+
+    print()
+    print(title)
+    print(artist)
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print("")
+    print(lyrics)
+    print("")
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print("")
+    input('Press Enter to quit')
+
+
+def search_lyrics():
+    title = input('Song Title: ')
+    artist = input('Artist: ')
+
+    def print_lyrics(song_title, song_singer):
+        return PyLyrics.getLyrics(song_singer, song_title)
+
+    try:
+        lyrics = print_lyrics(title, artist)
+    except ValueError:
+        lyrics = 'Lyrics not found :('
+
+    print()
+    print(title)
+    print(artist)
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print("")
+    print(lyrics)
+    print("")
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print("")
+    input('Press Enter to quit')
+
+
+def menu():
+    print('          Lyrixs')
+    print()
+    print('1. Spotify Lyrics')
+    print('2. Search any lyrics')
+    print()
+
+    def menu_action():
+        option = input('Option: ')
+
+        if option == '1':
+            spotify_lyrics()
+        elif option == '2':
+            search_lyrics()
+        else:
+            print('Invalid Option')
+            menu_action()
+
+    menu_action()
+
+
+menu()
---
+++ README.md
@@ -0,0 +1,48 @@
+# Lyrixs
+
+A simple app that shows you the Lyrics from the song you're listening on Spotify. 
+The songs come from the Lyrics Wikia database.
+You can also search for specific lyrics by the title of the song and the artist.
+
+
+### Prerequisites
+
+```
+Python 3.X
+PyLyrics
+```
+
+### Installing
+
+Clone the project directly from the GitHub Repo:
+
+```
+$ git clone https://github.com/rjchirinos/Lyrixs.git
+```
+
+After that just have to run Lyrixs.py from a terminal
+
+```
+$ python3 Lyrixs.py
+```
+
+
+### Running the tests
+
+A menu will be displayed:
+
+```
+ Lyrixs
+
+1. Spotify Lyrics
+2. Search any lyrics
+
+Option: 
+```
+If choose option 1 the app will automatically show the lyrics of the spotify currently playing song.
+With option 2 you will asked to input the song's name and the artist (Capitals and lowercase recognized)
+
+
+### Built With
+
+* [PyLyrics](https://github.com/geekpradd/PyLyrics) - The Module used to get the Lyrics from LyricsWikia
